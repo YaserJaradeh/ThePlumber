8ee9c22195a0db879080faf30eeaebced25100bb
Yaser Jaradeh <yaser.jaradeh@gmail.com>
2020-05-25 14:18:50 +0200

    Conform to new chain output class

--- auko/components/resolvers/base.py
+++ auko/components/resolvers/base.py
@@ -1,4 +1,6 @@
+from typing import List
 from auko.components import StanfordClient
+from auko.components.format import Chain
 
 
 class BaseResolver:
@@ -6,7 +8,7 @@ class BaseResolver:
     def __init__(self, name: str = 'Base Resolver'):
         self.name = name
 
-    def get_coreference_chains(self, text: str):
+    def get_coreference_chains(self, text: str) -> List[Chain]:
         pass
 
 
@@ -16,6 +18,6 @@ class StanfordBasedResolver(BaseResolver):
         super().__init__(name)
         self.client = client
 
-    def get_coreference_chains(self, text: str):
+    def get_coreference_chains(self, text: str) -> List[Chain]:
         pass
 
--- auko/components/resolvers/stanford.py
+++ auko/components/resolvers/stanford.py
@@ -1,5 +1,7 @@
+from typing import List
 from auko.components.stanford import StanfordClient
 from auko.components.resolvers.base import StanfordBasedResolver
+from auko.components.format import Chain
 
 
 class StanfordCoreferenceResolver(StanfordBasedResolver):
@@ -7,9 +9,16 @@ class StanfordCoreferenceResolver(StanfordBasedResolver):
     def __init__(self, stanford_client: StanfordClient):
         super().__init__(name='Stanford Coreference Resolver', client=stanford_client)
 
-    def get_coreference_chains(self, text):
-        x = self.client.coref(text)
-        # get sentence in output
-        # get location in string
-        # check if we need empty chains (or chains with only one occurrence)
-        return x
+    def get_coreference_chains(self, text) -> List[Chain]:
+        response = self.client.coref(text, simple_format=False)
+        result = []
+        for _, coref in response['corefs'].items():
+            if len(coref) > 1:
+                chain = Chain(coref[0]['text'])
+                for chunk in coref[1:]:
+                    sentence = response['sentences'][chunk['sentNum']-1]
+                    start = sentence['tokens'][chunk['startIndex']-1]['characterOffsetBegin']
+                    end = sentence['tokens'][chunk['endIndex']-2]['characterOffsetEnd']
+                    chain.add_alias(chunk['text'], start, end, text)
+                result.append(chain)
+        return result
