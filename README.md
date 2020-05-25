64b9911e85be287e0cb65b68b24812e925a967f7
Yaser Jaradeh <yaser.jaradeh@gmail.com>
2020-05-25 13:26:26 +0200

    Update requirements with spacy

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# AutoKomplete
+# AutoKomplete 
+a.k.a. AuKo
+
+AuKo is a framework for KG completion, it extracts triples from NL text and map the triples to their KG representations
 
 
--- auko/components/__init__.py
+++ auko/components/__init__.py
@@ -1,5 +1,10 @@
+# ===== Clients =====
 from .stanford import StanfordClient
 from .ollie import OLLIEClient
+# ===== Extractors =====
 from .extractors import BaseExtractor, StanfordBasedExtractor
 from .extractors import DependencyExtractor, OpenIEExtractor, KBPExtractor
 from .extractors import OllieExtractor, POSExtractor
+# ===== Resolvers =====
+from .resolvers import BaseResolver, StanfordBasedResolver
+from .resolvers import StanfordCoreferenceResolver, SpacyNeuralCoreferenceResolver
