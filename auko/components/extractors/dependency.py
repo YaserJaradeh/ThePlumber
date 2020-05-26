f7a7ac7335e0c3938aaae45b0711dd10bb880a54
Yaser Jaradeh <yaser.jaradeh@gmail.com>
2020-05-26 12:51:11 +0200

    Conform dependency extractor to Triple output

--- auko/components/extractors/dependency.py
+++ auko/components/extractors/dependency.py
@@ -1,6 +1,9 @@
+from typing import List
 from auko.components.stanford import StanfordClient
 from auko.components.extractors.base import StanfordBasedExtractor
+from auko.components.format import Triple
 from xml.dom.minidom import parseString
+import nltk
 
 
 # Adapted from https://github.com/anutammewar/extract_triplets/blob/1de00fe5f15be81bf8d8d02a32a9b00c4ec84882/extract.py
@@ -126,17 +129,26 @@ class DependencyExtractor(StanfordBasedExtractor):
         entity3 = string3
         return entity3
 
-    def __form_triplets(self, entity1, entity2):
+    def __form_triplets(self, entity1: str, entity2: str, sentence: str, previous_length: int, text: str):
         head = "-1"
         for tup in self.tuples:
             if "dobj" in tup[0] and tup[1] == self.root:
                 head = tup[2]
                 break
+        triple = Triple()
+        start_idx = sentence.find(entity1) + previous_length
+        end_idx = start_idx + len(entity1)
+        triple.add_subject(entity1, start_idx, end_idx, text)
+        ###########################
+        start_idx = sentence.find(entity2) + previous_length
+        end_idx = start_idx + len(entity2)
+        triple.add_predicate(entity2, start_idx, end_idx, text)
         if head != "-1":  # direct object present
-            entity3 = self.__get_object(head)  # get direct object entity
-            # w.write(str((entity1, entity2, entity3)) + "\n")
-            self.triplets.append((entity1, entity2, entity3))  # (entity1,entity2,direct-object)
-
+            entity3 = self.__get_object(head).strip()  # get direct object entity
+            start_idx = sentence.find(entity3) + previous_length
+            end_idx = start_idx + len(entity3)
+            triple.add_object(entity3, start_idx, end_idx, text)
+            self.triplets.append(triple)  # (entity1,entity2,direct-object)
             # if there is no other argument (other than direct-object) then go to next sentence
             if "nmod" not in self.rels:
                 if all(["nmod" not in k for k in self.rels.keys()]):
@@ -149,15 +161,18 @@ class DependencyExtractor(StanfordBasedExtractor):
         for rel in tuples1:
             if "nmod" in rel[0] and rel[1] == self.root:
                 head = rel[2]
-                entity3 = self.__get_object(head)
-                self.triplets.append((entity1, entity2, entity3))
-                # w.write(str((entity1, entity2, entity3)) + "\n")
+                entity3 = self.__get_object(head).strip()
+                start_idx = sentence.find(entity3) + previous_length
+                end_idx = start_idx + len(entity3)
+                triple.add_object(entity3, start_idx, end_idx, text)
+                self.triplets.append(triple)
         if head == "-1":
             return -1
         return 1
 
-    def __extract_triplets(self, sentences):
-        for sentence in sentences:
+    def __extract_triplets(self, sentences, text):
+        text_sentences = nltk.sent_tokenize(text)
+        for idx, sentence in enumerate(sentences):
             self.id_map = {}
             self.tree_up = {}
             self.tree_down = {}
@@ -189,19 +204,18 @@ class DependencyExtractor(StanfordBasedExtractor):
             entity1 = self.__combine_subj_words()
             if entity1 == -1:  # if the root word has no subject associated, skip the sentence
                 continue
-            if self.__form_triplets(entity1, entity2) == -1:
+            if self.__form_triplets(
+                    entity1.strip(),
+                    entity2.strip(),
+                    text_sentences[idx],
+                    sum([len(s) for s in text_sentences[:idx]]) + text[:text.find(text_sentences[idx]) + len(
+                        text_sentences[idx])].count(' ') - sum([s.count(' ') for s in text_sentences[:idx + 1]])
+                    , text) == -1:
                 continue
 
-    def get_triples(self, text):
+    def get_triples(self, text) -> List[Triple]:
         output = self.client.parse(text, output_format='xml')
         xmldoc = parseString(output)  # parse the xml output
         sentences = xmldoc.getElementsByTagName('sentence')
-        self.__extract_triplets(sentences)
-        triples = []
-        for triple in self.triplets:
-            triples.append({
-                'subject': triple[0],
-                'relation': triple[1],
-                'object': triple[2]
-            })
-        return triples
+        self.__extract_triplets(sentences, text)
+        return self.triplets
