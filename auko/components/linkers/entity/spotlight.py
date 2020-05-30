83f65705bd684e245bd3016d9905043615c09c60
Yaser Jaradeh <yaser.jaradeh@gmail.com>
2020-05-30 16:55:06 +0200

    Implement DBpedia Spotlight entity linker

---
+++ auko/components/linkers/entity/spotlight.py
@@ -0,0 +1,13 @@
+from auko.components.linkers.base import BaseEntityLinker, BaseWebLinker
+from typing import List, Tuple
+
+
+class DBpediaSpotlightEntityLinker(BaseEntityLinker, BaseWebLinker):
+
+    def __init__(self):
+        BaseEntityLinker.__init__(self, name="DBpedia Spotlight linker")
+        BaseWebLinker.__init__(self, api_url="https://api.dbpedia-spotlight.org/en/annotate")
+
+    def get_entities(self, text: str) -> List[Tuple[str, str]]:
+        result = self.client.GET(params={'text': text}, verify=False, headers={"accept": "application/json"}).json()
+        return [(entity['@URI'], entity['@surfaceForm']) for entity in result['Resources']]
