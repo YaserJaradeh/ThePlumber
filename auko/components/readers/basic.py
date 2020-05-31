bb82599a8306b06d4b77c02fed11dde334918a2e
Yaser Jaradeh <yaser.jaradeh@gmail.com>
2020-05-31 13:25:28 +0200

    Fix missing args to inherited methods

--- auko/components/readers/basic.py
+++ auko/components/readers/basic.py
@@ -8,7 +8,7 @@ class StandardReader(BaseReader):
     def __init__(self):
         super().__init__(name='Reader from standard input (console)')
 
-    def read(self) -> AnyStr:
+    def read(self, **kwargs) -> AnyStr:
         return input('Please enter the input text')
 
 
@@ -17,7 +17,7 @@ class RawFileReader(BaseReader):
     def __init__(self):
         super().__init__(name='Reader from a file')
 
-    def read(self, file_path) -> AnyStr:
+    def read(self, file_path, **kwargs) -> AnyStr:
         if not exists(file_path):
             raise ValueError(f"File path provided is non existence ({file_path})")
         with open(file_path, 'r') as input_file:
