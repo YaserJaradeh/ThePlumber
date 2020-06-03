d0ac58ffb5918e8fbaa2e3ba9b09cc72e8bbc8a7
Yaser Jaradeh <yaser.jaradeh@gmail.com>
2020-06-03 23:21:14 +0200

    Test complex pipeline with aggregators

--- auko/pipeline/test_pipeline.py
+++ auko/pipeline/test_pipeline.py
@@ -1,6 +1,8 @@
 from consecution import Node, Pipeline, GlobalState
-from auko.nodes.nodes import ExtractionNode, ReadingNode, ResolutionNode, ProcessingNode, WritingNode, JointLinkingNode
-from auko.components import StandardReader, OpenIEExtractor, StanfordClient, SpacyNeuralCoreferenceResolver, StandardWriter, EARLJointLinker
+from auko.nodes.nodes import ExtractionNode, ReadingNode, ResolutionNode, ProcessingNode, WritingNode, JointLinkingNode, \
+    AggregationNode
+from auko.components import StandardReader, OpenIEExtractor, StanfordClient, SpacyNeuralCoreferenceResolver, \
+    StandardWriter, EARLJointLinker, DependencyExtractor, FalconJointLinker
 
 
 # This is the same node class we defined above
@@ -15,9 +17,13 @@ if __name__ == '__main__':
     client = StanfordClient()
     pipe = Pipeline(
         ReadingNode('Reader', StandardReader())
-        | [(ExtractionNode('Extractor', OpenIEExtractor(client)) | JointLinkingNode('Linker', EARLJointLinker())),
-           ResolutionNode('Resolution', SpacyNeuralCoreferenceResolver())]
-        | ProcessingNode('Processer')
+        | [([ExtractionNode('Extractor 1', OpenIEExtractor(client)),
+             ExtractionNode('Extractor 2', DependencyExtractor(client))]
+            | AggregationNode('Collector 1')
+            | ([JointLinkingNode('Linker 1', EARLJointLinker()), JointLinkingNode('Linker 2', FalconJointLinker())])
+            | AggregationNode('Collector 2')),
+           ResolutionNode('Resolver', SpacyNeuralCoreferenceResolver())]
+        | ProcessingNode('Processor')
         | WritingNode('Writer', StandardWriter()),
         global_state=GlobalState(triples=[], caller=None)
     )
