6432fa93e80c1a755b8fd880745095a3f793e28a
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-06-15 17:01:49 +0200

    Update linkers base constructors to accept kwargs

--- auko/components/linkers/base/base.py
+++ auko/components/linkers/base/base.py
@@ -6,7 +6,7 @@ class BaseEntityLinker(AuKoClass):
     """
     Base entity linking class, other entity linkers will inherit this one
     """
-    def __init__(self, name: str = 'Base Entity Linker'):
+    def __init__(self, name: str = 'Base Entity Linker', **kwargs):
         self.name = name
 
     def get_entities(self, text: str) -> List[Tuple[str, str]]:
@@ -17,7 +17,7 @@ class BaseRelationLinker(AuKoClass):
     """
     Base relation linking class, other relation linkers will inherit this one
     """
-    def __init__(self, name: str = 'Base Relation Linker'):
+    def __init__(self, name: str = 'Base Relation Linker', **kwargs):
         self.name = name
 
     def get_relations(self, text: str) -> List[Tuple[str, str]]:
@@ -28,7 +28,7 @@ class BaseJointLinker(AuKoClass):
     """
     Base joint linking class, other joint linkers will inherit this one
     """
-    def __init__(self, name: str = 'Base Joint (relation and entity) Linker'):
+    def __init__(self, name: str = 'Base Joint (relation and entity) Linker', **kwargs):
         self.name = name
 
     def get_entities_and_relations(self, text: str) -> Tuple[List[Tuple[str, str]], List[Tuple[str, str]]]:
@@ -40,11 +40,11 @@ class DummyLinker(AuKoClass):
     Dummy Linker used for testing purposes or to fill space in the pipeline
     """
 
-    def __init__(self, name: str = 'Dummy Linker'):
+    def __init__(self, name: str = 'Dummy Linker', **kwargs):
         self.name = name
 
     def get_entities_and_relations(self, text: str) -> Tuple[List[Tuple[str, str]], List[Tuple[str, str]]]:
-        return []
+        return [], []
 
     def get_relations(self, text: str) -> List[Tuple[str, str]]:
         return []
