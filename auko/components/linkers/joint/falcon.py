45c6b57c58cd89fcdc513cb6358f212a3d39aeba
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-06-17 10:28:12 +0200

    Catch exception if no response is returned in FALCON

--- auko/components/linkers/joint/falcon.py
+++ auko/components/linkers/joint/falcon.py
@@ -1,3 +1,5 @@
+from json import JSONDecodeError
+
 from auko.components.linkers.base import BaseLinker, BaseWebLinker
 from typing import List, Tuple
 
@@ -17,12 +19,16 @@ class FalconJointLinker(BaseLinker, BaseWebLinker):
         params = {"mode": mode}
         if db == 'dbpedia':
             params['db'] = 1
-        result = self.client.POST(json={"text": text}, params=params).json()
-        ent_key = f'entities_{kg}'
-        rel_key = f'relations_{kg}'
-        entities = [(entity[0][1:-1], entity[1], 'entity') for entity in result[ent_key]] if ent_key in result else []
-        relations = [(relation[0][1:-1], relation[1], 'relation') for relation in result[rel_key]] if rel_key in result else []
-        return entities + relations
+        try:
+            result = self.client.POST(json={"text": text}, params=params).json()
+            ent_key = f'entities_{kg}'
+            rel_key = f'relations_{kg}'
+            entities = [(entity[0][1:-1], entity[1], 'entity') for entity in result[ent_key]] if ent_key in result else []
+            relations = [(relation[0][1:-1], relation[1], 'relation') for relation in result[rel_key]] if rel_key in result else []
+            return entities + relations
+        except JSONDecodeError as ex:
+            print(ex)
+            return []
 
 
 class FalconDBpediaJointLinker(FalconJointLinker):
