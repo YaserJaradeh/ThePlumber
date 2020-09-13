0582a1c9a0543cea32d6fd293c9393d28d6f8f16
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-09-13 18:26:53 +0200

    Rewrite MinIE as service, cf. https://github.com/pminervini/minie-1/commit/a80ff5e52eba571cbb8704b7da17be93c7bd17d4

--- plumber/components/extractors/minie.py
+++ plumber/components/extractors/minie.py
@@ -1,8 +1,10 @@
 from typing import List
 from plumber.components.extractors.base import BaseExtractor
 from plumber.components.format import Triple
-import os
-import inspect
+import requests
+
+
+MINIE_URL = "http://localhost:8080/minie/query"
 
 
 class MinIEExtractor(BaseExtractor):
@@ -11,59 +13,48 @@ class MinIEExtractor(BaseExtractor):
         super().__init__(name='MinIE extractor', **kwargs)
 
     def get_triples(self, text) -> List[Triple]:
-        # Generate the extractions (With SAFE mode (mode = 2))
-        # NOTE: sentence must be wrapped into String, else it won't work.
-        minie = MinIE(String(sentence), parser, 2)
+
+        payload = text
+        headers = {
+            'Content-Type': 'text/plain'
+        }
+
+        response = requests.request("POST", MINIE_URL, headers=headers, data=payload)
+        facts = response.json()['facts']
+
         triples = []
-        for ap in minie.getPropositions().elements():
-            # Some elements might by null so we don't process them.
-            if ap is not None:
-                triple = Triple()
-                # ========== Subject ==============
-                if ap.subject.toString() not in text:
-                    start_index = -1
-                    end_index = -1
-                else:
-                    start_index = text.index(ap.subject.toString())
-                    end_index = start_index + len(ap.subject.toString())
-                triple.add_subject(ap.subject.toString(), start_index, end_index, text)
-                # ========== Predicate ==============
-                if ap.relation.toString() not in text:
-                    start_index = -1
-                    end_index = -1
-                else:
-                    start_index = text.index(ap.relation.toString())
-                    end_index = start_index + len(ap.relation.toString())
-                triple.add_predicate(ap.relation.toString(), start_index, end_index, text)
-                # ========== Object ==============
-                if ap.object.toString() not in text:
-                    start_index = -1
-                    end_index = -1
-                else:
-                    start_index = text.index(ap.object.toString())
-                    end_index = start_index + len(ap.object.toString())
-                triple.add_object(ap.object.toString(), start_index, end_index, text)
-                # ========== Add triple to final output
-                triples.append(triple)
+        for fact in facts:
+            triple = Triple()
+            # ========== Subject ==============
+            subj = fact['subject']
+            if subj not in text:
+                start_index = -1
+                end_index = -1
+            else:
+                start_index = text.index(subj)
+                end_index = start_index + len(subj)
+            triple.add_subject(subj, start_index, end_index, text)
+            # ========== Predicate ==============
+            pred = fact['predicate']
+            if pred not in text:
+                start_index = -1
+                end_index = -1
+            else:
+                start_index = text.index(pred)
+                end_index = start_index + len(pred)
+            triple.add_predicate(pred, start_index, end_index, text)
+            # ========== Object ==============
+            obj = fact['object']
+            if obj not in text:
+                start_index = -1
+                end_index = -1
+            else:
+                start_index = text.index(obj)
+                end_index = start_index + len(obj)
+            triple.add_object(obj, start_index, end_index, text)
+            # ========== Add triple to final output
+            triples.append(triple)
         return triples
-
-
-# Change CLASSPATH to point to the minie jar archive
-os.environ['CLASSPATH'] = f"{os.path.dirname(inspect.getfile(MinIEExtractor))}/resources/minie-0.0.1-SNAPSHOT.jar"
-
-# Set JAVA_HOME for this session
-os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64/'
-
-from jnius import autoclass
-
-CoreNLPUtils = autoclass('de.uni_mannheim.utils.coreNLP.CoreNLPUtils')
-AnnotatedProposition = autoclass('de.uni_mannheim.minie.annotation.AnnotatedProposition')
-MinIE = autoclass('de.uni_mannheim.minie.MinIE')
-StanfordCoreNLP = autoclass('edu.stanford.nlp.pipeline.StanfordCoreNLP')
-String = autoclass('java.lang.String')
-
-# Dependency parsing pipeline initialization
-parser = CoreNLPUtils.StanfordDepNNParser()
 
 
 if __name__ == "__main__":
