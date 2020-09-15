a74623af4d892520c57cc81d18f9d79c3c5543b0
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-09-15 15:59:28 +0200

    Update links to remote ANN endpoints

--- plumber/components/linkers/joint/spacy_ann.py
+++ plumber/components/linkers/joint/spacy_ann.py
@@ -4,13 +4,13 @@ from plumber.components.linkers.base import BaseSpacyANNLinker
 class DBpediaSpacyANNJointLinker(BaseSpacyANNLinker):
 
     def __init__(self, **kwargs):
-        super().__init__(url='http://127.0.0.1:11222', allowed_relation_types=['DB-PRP', 'DB-ONT'])
+        super().__init__(url='http://node4.research.tib.eu:11222', allowed_relation_types=['DB-PRP', 'DB-ONT'])
 
 
 class WikidataSpacyANNJointLinker(BaseSpacyANNLinker):
 
     def __init__(self, **kwargs):
-        super().__init__(url='http://127.0.0.1:11333', allowed_relation_types=['WD-PRP'])
+        super().__init__(url='http://node4.research.tib.eu:11333', allowed_relation_types=['WD-PRP'])
 
 
 if __name__ == '__main__':
