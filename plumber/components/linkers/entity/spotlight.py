b4d5ce317f78a7fb359e6b4226c15e5358fead3d
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-09-17 10:07:23 +0200

    Switch spotlight to local endpoint and add safety check

--- plumber/components/linkers/entity/spotlight.py
+++ plumber/components/linkers/entity/spotlight.py
@@ -7,10 +7,18 @@ from typing import List
 class DBpediaSpotlightEntityLinker(BaseLinker, BaseWebLinker):
 
     def __init__(self, **kwargs):
-        kwargs['api_url'] = "https://api.dbpedia-spotlight.org/en/annotate"
+        kwargs['api_url'] = "http://localhost:33311/rest/annotate"
         BaseLinker.__init__(self, name="DBpedia Spotlight linker", **kwargs)
         BaseWebLinker.__init__(self, **kwargs)
 
     def get_links(self, text: str) -> List[Pair]:
         result = self.client.GET(params={'text': text}, verify=False, headers={"accept": "application/json"}).json()
-        return [Pair(entity['@URI'], entity['@surfaceForm'], 'entity') for entity in result['Resources']]
+        if 'Resources' in result:
+            return [Pair(entity['@URI'], entity['@surfaceForm'], 'entity') for entity in result['Resources']]
+        else:
+            return []
+
+
+if __name__ == '__main__':
+    linker = DBpediaSpotlightEntityLinker()
+    x = linker.get_links("Barack Obama was the president of the USA.")
