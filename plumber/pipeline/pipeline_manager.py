7b89bbbfba28dab9254af10f7abee1039e435fc9
Salomon Kabongo <salomon@aims.ac.za>
2020-09-28 11:32:27 +0200

    working on an other extractor

--- plumber/pipeline/pipeline_manager.py
+++ plumber/pipeline/pipeline_manager.py
@@ -231,29 +231,33 @@ class PipelineParser:
 
 
 if __name__ == '__main__':
+    
+    pipe = PipelineParser.parse('config.yml')
+    pipe.consume([1])
+    
     #pipe = PipelineParser.parse('config.yml')
     #pipe.plot()
-    resources = ["R12220", "R12223", "R12226", "R12231", "R12233", "R12235", "R12237", "R12241", "R12243", "R12245", "R12247", "R25005", "R36109", "R36114", "R36123", "R36138", "R36151", "R37001", "R37003", "R37006", "R37008"]
-    config = {
-        "pipeline": {
-            "name": "test",
-            "components": {
-                "extractor": "user",
-                "linker": "dummy",
-                "resolver": "dummy",
-                "reader": "raw_file",
-                "writer": "file"
-            },
-            "parameters": {
-                "input_file": './text/R36138.txt',
-                "output_file": './text/R36138-triples.txt'
-            }
-        }
-    }
-    for resource in resources:
-        config["pipeline"]["parameters"]["input_file"] = f"./text/{resource}.txt"
-        config["pipeline"]["parameters"]["output_file"] = f"./text/{resource}-triples.txt"
-        pipeline, params = PipelineParser.create(config)
-        pipeline.consume([1])
-        PipelineParser.clean_up(params)
+    # resources = ["R12220", "R12223", "R12226", "R12231", "R12233", "R12235", "R12237", "R12241", "R12243", "R12245", "R12247", "R25005", "R36109", "R36114", "R36123", "R36138", "R36151", "R37001", "R37003", "R37006", "R37008"]
+    # config = {
+    #     "pipeline": {
+    #         "name": "test",
+    #         "components": {
+    #             "extractor": "user",
+    #             "linker": "dummy",
+    #             "resolver": "dummy",
+    #             "reader": "raw_file",
+    #             "writer": "file"
+    #         },
+    #         "parameters": {
+    #             "input_file": './text/R36138.txt',
+    #             "output_file": './text/R36138-triples.txt'
+    #         }
+    #     }
+    # }
+    # for resource in resources:
+    #     config["pipeline"]["parameters"]["input_file"] = f"./text/{resource}.txt"
+    #     config["pipeline"]["parameters"]["output_file"] = f"./text/{resource}-triples.txt"
+    #     pipeline, params = PipelineParser.create(config)
+    #     pipeline.consume([1])
+    #     PipelineParser.clean_up(params)
 
